14e31b1a14503f0b7e80d0f73451f2b5283a1555
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-11-23 11:18:06 -0800

    internal/pkg/exec: compile on windows

---
+++ internal/pkg/exec/exec_windows.go
@@ -0,0 +1,20 @@
+// +build windows
+
+package exec
+
+import (
+	"os"
+	"os/exec"
+)
+
+func realExec(argv0 string, argv []string, envv []string) error {
+	// Since Windows doesn't support fork/exec, we just run the process
+	// and keep our parent process around. This isn't identical by any means
+	// but for our usage it generally works.
+	cmd := exec.Command(argv0, argv...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Env = envv
+	return cmd.Run()
+}
